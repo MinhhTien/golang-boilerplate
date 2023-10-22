fab7b9f161049547eeee75339c371484a9cc5a1b
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-10-22 22:39:26 +0700

    Add seeder

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -25,15 +25,40 @@ func Load(db *gorm.DB) {
 	}
 
 	users := []models.User{
-		models.User{
+		{
 			Username: "stev",
 			Email:    "stev@gmail.com",
 			Password: "stev123",
-		   },
-		   models.User{
+		},
+		{
 			Username: "martin",
 			Email:    "martin@gmail.com",
 			Password: "martin123",
-		   },
+		},
+	}
+
+	todos := []models.ToDo{
+		{
+			Title:   "Dance class",
+			Content: "I have to find dance class near me",
+		},
+		{
+			Title:   "Coding",
+			Content: "I have to learn coding",
+		},
+	}
+
+	for i := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			// Log the error if it occurs
+			log.Fatalf("Cannot seed users table: %v", err)
+		}
+		todos[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.ToDo{}).Create(&todos[i]).Error
+		if err != nil {
+			log.Fatalf("Cannot seed todos table: %v", err)
+		}
 	}
 }
